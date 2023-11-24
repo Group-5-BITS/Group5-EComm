f63ff7ee07ac21484d800e877659485eb970d0a9
Aditi Agarwal <aditi.agarwal@sap.com>
2023-11-24 20:41:37 +0530

    modified directory in jenkinsfile for maven

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+# Navigate to the 'clients' folder
+cd clients || exit
+
+# Run Maven command to get project name
+NAME=$(mvn help:evaluate -Dexpression=project.name | grep "^[^\[]")
 set +x
 
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+# Run Maven command to get project version
+VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
 set +x
 
 set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
-
-set -x
+# Run Java command to execute the JAR file
 java -jar target/${NAME}-${VERSION}.jar
